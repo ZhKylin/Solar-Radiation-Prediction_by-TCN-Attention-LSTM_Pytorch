8396c1908b104c4f4833616c4c5214da854b2df4
ZKylin <z_kylin@sjtu.edu.cn>
2024-06-07 23:52:57 +0800

    Added pre-processing programs

---
+++ 1_dataset/dataset.py
@@ -0,0 +1,77 @@
+# Encoding: UTF-8
+# Author: Kylin Zhang
+# Time: 2024/5/27 - 20:23
+
+# step2：读取处理后的表格并进行训练集、验证集和测试集划分
+
+
+import os
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# 参数
+train_ratio = 0.8  # 训练集占比
+val_ratio = 0.1  # 验证集占比
+batch_size = 128  # 批处理大小(即一次用多少个连续数据预测一下数据)
+
+# 读取表格文件
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(current_dir, 'SolarPrediction_processed.csv')
+dataset = pd.read_csv(data_path)
+
+# 特征选取
+dataset = dataset[['Radiation', 'Temperature', 'Pressure',
+                   'Humidity', 'WindDirection(Degrees)',
+                   'Speed', 'DayOfYear', 'TimeOfDay(s)', 'DayLength(s)']]
+
+# 训练集和验证集划分
+# 训练集 ：验证集 ：测试集 = 8 ：1 ：1
+train = dataset.iloc[0:int(len(dataset) * train_ratio), :]
+val = dataset.iloc[int(len(dataset) * train_ratio):
+                   int(len(dataset) * (train_ratio + val_ratio)), :]
+test = dataset.iloc[int(len(dataset) * (train_ratio + val_ratio)):, :]
+
+
+# 数据归一化
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler.fit_transform(dataset)
+train = scaler.transform(train)
+val = scaler.transform(val)
+test = scaler.transform(test)
+
+
+# 特征和标签划分
+# 用每batch_size的数据预测紧邻的下一个值
+x_train = []
+y_train = []
+x_val = []
+y_val = []
+x_test = []
+y_test = []
+for i in range(batch_size, len(train)):
+    x_train.append(train[i - batch_size:i, :])
+    y_train.append(train[i, 0])
+for i in range(batch_size, len(val)):
+    x_val.append(val[i - batch_size:i, :])
+    y_val.append(val[i, 0])
+for i in range(batch_size, len(test)):
+    x_test.append(test[i - batch_size:i, :])
+    y_test.append(test[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_val, y_val = np.array(x_val), np.array(y_val)
+x_test, y_test = np.array(x_test), np.array(y_test)
+y_train, y_val, y_test = \
+    (y_train.reshape(-1, 1), y_val.reshape(-1, 1), y_test.reshape(-1, 1))
+
+
+if __name__ == '__main__':
+    print(x_train.shape, y_train.shape)
+    print(x_val.shape, y_val.shape)
+    print(x_test.shape, y_test.shape)
+
+    # data_range = scaler.data_range_
+    # data_min = scaler.data_min_
+    # y_test = y_test * data_range[0] + data_min[0]
+    # print(y_test)
---
+++ 1_dataset/pre_process.py
@@ -0,0 +1,45 @@
+# Encoding: UTF-8
+# Author: Kylin Zhang
+# Time: 2024/5/27 - 16:49
+
+# step1：对原始数据表格进行特征提取，得到文件SolarPrediction_processed.csv
+
+
+import pandas as pd
+from pytz import timezone
+import pytz
+
+# 读取表格文件
+dataset = pd.read_csv('SolarPrediction_original.csv')
+dataset = dataset.sort_values(['UNIXTime'], ascending=True)
+dataset = dataset.ffill()
+
+# 对表格时间数据进行处理
+# 将时间戳改为日期作为索引，并将时区转化为当地时区
+hawaii = timezone('Pacific/Honolulu')
+dataset.index = pd.to_datetime(dataset['UNIXTime'], unit='s')
+dataset.index = dataset.index.tz_localize(pytz.utc).tz_convert(hawaii)
+# 将日期拆分到新列，并删除原日期列
+dataset['MonthOfYear'] = dataset.index.strftime('%m').astype(int)
+dataset['WeekOfYear'] = dataset.index.strftime('%U').astype(int)
+dataset['DayOfYear'] = dataset.index.strftime('%j').astype(int)
+dataset.drop('Data', inplace=True, axis=1)
+# 将时间拆分到新列，并删除原时间列
+dataset['TimeOfDay(h)'] = dataset.index.hour
+dataset['TimeOfDay(m)'] = dataset.index.hour*60 + dataset.index.minute
+dataset['TimeOfDay(s)'] = (dataset.index.hour*60*60 + dataset.index.minute*60
+                           + dataset.index.second)
+dataset.drop('Time', inplace=True, axis=1)
+# 将日出日落时间换为当天日照时长，并删除原日出日落时间列
+dataset['TimeSunRise'] = pd.to_datetime(dataset['TimeSunRise'], format='%H:%M:%S')
+dataset['TimeSunSet'] = pd.to_datetime(dataset['TimeSunSet'], format='%H:%M:%S')
+dataset['DayLength(s)'] = (dataset['TimeSunSet'].dt.hour*60*60
+                           + dataset['TimeSunSet'].dt.minute*60
+                           + dataset['TimeSunSet'].dt.second
+                           - dataset['TimeSunRise'].dt.hour*60*60
+                           - dataset['TimeSunRise'].dt.minute*60
+                           - dataset['TimeSunRise'].dt.second)
+dataset.drop(['TimeSunRise', 'TimeSunSet'], inplace=True, axis=1)
+
+# 存储新列表
+dataset.to_csv('SolarPrediction_processed.csv', index=False)
